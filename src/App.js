daace941f32e040bc36c25b005fbac757c498849
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:27 +0530

    Initial commit: Instagram-Mobile

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Instagram-Mobile"</h1>
+        <p>"Cross-platform social media"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
